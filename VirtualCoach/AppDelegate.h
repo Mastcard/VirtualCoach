12ebb2c2cfd5292bbf42d1ea626b08807664c07e
itzseven <iSeven@macbook-pro-de-romain-2.local>
2016-02-20 20:35:17 +0100

    Initial Commit

---
+++ VirtualCoach/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VirtualCoach
+//
+//  Created by Romain Dubreucq on 20/02/2016.
+//  Copyright © 2016 Romain Dubreucq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VirtualCoach/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VirtualCoach
+//
+//  Created by Romain Dubreucq on 20/02/2016.
+//  Copyright © 2016 Romain Dubreucq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
