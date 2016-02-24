1c2385021d8eaf4902682f33468c1f7e138c39c9
Romain Dubreucq <iSeven@MacBook-Pro-de-Romain-2.local>
2016-02-24 15:33:40 +0100

    Add folder architecture

---
+++ VirtualCoach/Classes/Engine/Image/Tracking/c/charact_ext.c
@@ -0,0 +1,9 @@
+//
+//  charact_ext.c
+//  VirtualCoach
+//
+//  Created by Romain Dubreucq on 23/02/2016.
+//  Copyright © 2016 itzseven. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ VirtualCoach/Classes/Engine/Image/Tracking/c/include/charact_ext.h
@@ -0,0 +1,13 @@
+//
+//  charact_ext.h
+//  VirtualCoach
+//
+//  Created by Romain Dubreucq on 23/02/2016.
+//  Copyright © 2016 itzseven. All rights reserved.
+//
+
+#ifndef charact_ext_h
+#define charact_ext_h
+
+
+#endif /* charact_ext_h */
