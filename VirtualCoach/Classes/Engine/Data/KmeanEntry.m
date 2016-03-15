abc772ef4bce95cf79621e9c58b3fef6949c1c82
Bi ZORO <bizoro@macbook-pro.local>
2016-03-15 15:23:27 +0100

    Remove bullshit's code

--- VirtualCoach/Classes/Engine/Data/KmeanEntry.m
+++ VirtualCoach/Classes/Engine/Data/KmeanEntry.m
@@ -19,53 +19,6 @@
     }
     return  self;
 }
-/*
-- (void)generateDataEntryForKmeanFromFirstSpeedVectorsTab:(speedVector *)speed1 andSecondSpeedVectorsTab:(speedVector *)speed2 betweenInterval:(rect_t)interval andWithImageWidth:(uint16_t)width{
-    
-    NSMutableArray * tmpHistogram = [[NSMutableArray alloc] initWithCapacity:360];
-    for (NSInteger i=0; i<360; i++) {
-        [tmpHistogram insertObject:@0 atIndex:i];
-    }
-    
-    double cumulAcceleration =0;
-    unsigned int nbAcceleration =0;
-    
-    unsigned int minHit =0;
-    
-    double u1 = 0, u2 = 0, v1 = 0, v2= 0;
-    double angle = 0;
-    double norm1 = 0, norm2 =0;
-    for (NSInteger y=interval.start.y; y<interval.end.y; y++) {
-        for (NSInteger x=interval.start.x; x<interval.end.x; x++){
-            u2= speed2[PXL_IDX(width, x, y)].u;
-            v2= speed2[PXL_IDX(width, x, y)].v;
-            norm2 = sqrt(u2*u2 + v2*v2);
-            
-            u1= speed1[PXL_IDX(width, x, y)].u;
-            v1= speed1[PXL_IDX(width, x, y)].v;
-            norm1 = sqrt(u1*u1 + v1*v1);
-            if (norm2 > 0.00){
-                angle = atan2(-v2, u2) * 180 / M_PI;
-                if (angle < 0) {
-                    angle += 360;
-                }
-                tmpHistogram[(unsigned int) angle] = @([[tmpHistogram objectAtIndex:(unsigned int) angle] intValue] + 1);
-            }
-            cumulAcceleration += norm2 - norm1;
-            nbAcceleration ++;
-        }
-    }
-    _meanAcceleration = cumulAcceleration / nbAcceleration;
-    
-    for (NSInteger i=0; i<360; i++) {
-        if ([[tmpHistogram objectAtIndex:i] intValue] > minHit) {
-            minHit = [[tmpHistogram objectAtIndex:i] intValue];
-            _maxAngle = (unsigned int)i;
-        }
-    }
-    [tmpHistogram removeAllObjects];
-}
-*/
 
 - (void)generateDataEntryForKmeanFromFirstSpeedVectorsTab:(speedVector *)speed1 andSecondSpeedVectorsTab:(speedVector *)speed2 betweenInterval:(rect_t)interval andWithImageWidth:(uint16_t)width{
     
